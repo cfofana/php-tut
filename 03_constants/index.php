069e54bfa05b8c74f17fd50552efea8098d08a9b
Ceesay Fofana <22731598+cfofana@users.noreply.github.com>
2023-08-09 23:49:13 -0400

    Define constants and output to screen

---
+++ 03_constants/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP | Constants</title>
+</head>
+<body>
+    <?php
+        define(PI, 3.14);
+        define(BOOK_TITLE, "My love");
+        echo PI . "<br/>";
+        echo BOOK_TITLE;
+    ?>
+</body>
+</html>
