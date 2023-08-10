100aee17c6dbf9028e4f5d39fbc18f44836fbe30
Ceesay Fofana <22731598+cfofana@users.noreply.github.com>
2023-08-09 23:48:47 -0400

    Declare variables and output to screen

---
+++ 02_variables/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP | Variables</title>
+</head>
+<body>
+    <?php 
+        $vehicles = "subaru";
+        $number = 1;
+        $isTrue = true;
+        echo "$vehicles, <br/> ";
+
+        $name = "Ceesay";
+        $age = 21;
+        $color = "purple";
+        print "Hello, my name is $name, I am
+        $age years old. I like $color.";
+    ?>
+</body>
+</html>
