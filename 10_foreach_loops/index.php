a1046a8e95362ae04bab9746c92088b6b5c4317e
Ceesay Fofana <22731598+cfofana@users.noreply.github.com>
2023-08-14 23:26:45 -0400

    Practice foreach loops

---
+++ 10_foreach_loops/index.php
@@ -0,0 +1,54 @@
+<?php
+	
+	// Constants
+	define("TITLE", "Foreach Loop");
+	
+	// Custom Variables
+	$name	= "Ceesay";
+	$lesson_num	= 10;
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PHP <?php echo TITLE; ?></title>
+		<link href="../assets/styles.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="wrapper">
+			<a href="/" title="Back to directory" id="logo">
+				<img src="../assets/img/logo.png" alt="PHP">
+			</a>
+			
+			<h1>Tutorial <?php echo $lesson_num; ?>: <small><?php echo TITLE; ?></small></h1>
+			<hr>
+			
+			<h2>Your Example</h2>
+			
+			<div class="sandbox">
+				
+				<?php
+				 
+				    $moustaches = ["Handlebar", "Fu Manchu", "Salvador Dali"];
+                    foreach($moustaches as $value){
+                        //echo $value."<br/>";
+                        echo "$value <br/>";
+                    }
+				 
+				?>
+				
+			</div><!-- end sandbox -->
+			
+			<a href="index.php" class="button">Back to the lecture</a>
+			
+			<hr>
+			
+			<small>&copy;<?php echo date('F j, Y'); ?> - <?php echo $name; ?></small>
+		</div><!-- end wrapper -->
+		
+		<div class="copyright-info">
+			<?php include('../assets/includes/copyright.php'); ?>
+		</div><!-- end copyright-info -->
+	</body>
+</html>
