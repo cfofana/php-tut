2e339ac968c31b57c38f0774c10d0a8d7338fb0d
Ceesay Fofana <22731598+cfofana@users.noreply.github.com>
2023-08-24 21:36:41 -0400

    Begin template for dishes and populate menu page with text

---
+++ Final_Project/dish.php
@@ -0,0 +1,8 @@
+<?php 
+    define("TITLE", "Menu Item | Franklin's Fine Dining");
+
+    include("assets/includes/header.php");
+
+
+    include("assets/includes/footer.php");
+?>
--- Final_Project/menu.php
+++ Final_Project/menu.php
@@ -2,6 +2,26 @@
 <?php include("assets/includes/header.php") ?>
 
 
+<div class="menu-items">
+    <h1>Our Delicious Menu</h1>
+    <p>Like our team, our menu is very small &mdash; but dang, 
+        does it pack a punch.
+    </p>
+    <p><em>Click any menu item to learn more about it.</em></p>
+    <div class="img-separator">
+        <img src="assets/img/hr.png" alt="A decorative horizontal ruler">
+    </div>
 
+    <ul>
+        <?php 
+            foreach($food_menu_items as $dish => $item){
+         ?>
+            <li><a href="dish.php?item=<?php echo $dish; ?>"><?php echo $item[title]; ?></a><sup>  $</sup><?php echo $item[price]; ?></li>
+
+        <?php     
+        }
+        ?>
+    </ul>
+</div>
 
 <?php include("assets/includes/footer.php") ?>
