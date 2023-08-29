f6a8c3aba80b7db4d8efdfe89edbad43f1c099e9
Ceesay Fofana <22731598+cfofana@users.noreply.github.com>
2023-08-28 23:41:50 -0400

    Finish contact page and style it

--- Final_Project/contact.php
+++ Final_Project/contact.php
@@ -8,6 +8,51 @@
             <img src="assets/img/hr.png" alt="A decorative horizontal ruler">
         </div>
         <h1>Get in touch with you</h1>
+
+<?php 
+    if(isset($_POST["contact_submit"])){
+        $name = trim($_POST["name"]);
+        $email = trim($_POST["email"]);
+        $msg = $_POST["message"];
+
+        function has_header_injection($str){
+           return preg_match("/[\r\n]/", $str); 
+        }
+
+        if(has_header_injection($name) || has_header_injection($email)){
+            die();
+        }
+        if(!$name || !$email || !$msg){
+            echo "<h4 class='error-msg'>All fields required.</h4><a href='contact.php' class='btn-go-back'>Go back and try again.</a>";
+            exit;
+        }
+        // Add the recipient email
+        $to = "ceesayfofana@hotmail.co.uk";
+        $subject = "$name sent you a message";
+        $message = "Name: $name\r\n";
+        $message .= "Email: $email\r\n";
+        $message .= "Message:\r\n$msg";
+
+        if(isset($_POST["subscribe"]) && $_POST["subscribe"] == "Subscribe"){
+            // Add a new line to message variable
+            $message .= "\r\n\r\n\r\nPlease add $email to your mailing list.";
+        }
+        $message = wordwrap(72);
+        $headers = "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+        $headers .= "From: $name <$email>\r\n";
+        $headers .= "X-Priority: 1\r\n";
+        $headers .= "X-MSMail-Priority: High\r\n\r\n";
+
+        // Send the email
+         mail($to, $subject, $message, $headers);
+?>
+        <h5>Thanks for contacting Franklin's.</h5>
+        <p>Please allow 24 hours for a response.</p>
+        <p><a href="index.php" class="btn-homepage">&laquo; Go Back to Home page</a></p>
+<?php
+    } else {
+?>
         <form method="post" action="">
             <label for="name">Your Name</label><br>
             <input type="text" name="name" id="name"><br>
@@ -23,7 +68,7 @@
 
             <input type="submit" class="btn-submit" name="contact_submit" value="Send Message">        
         </form>
-
+<?php } ?>
         <div class="img-separator">
             <img src="assets/img/hr.png" alt="A decorative horizontal ruler">
         </div>
